93153b7d48d080186bdae201ed77ccb0ab272cf1
Pramuditha <pramuditha.niroshan@circles.asia>
2022-08-20 16:24:45 +0530

    Type check for number prop

--- react-typescript-demo/src/App.tsx
+++ react-typescript-demo/src/App.tsx
@@ -6,7 +6,7 @@ import { Greet } from './components/Greet';
 function App() {
   return (
     <div className="App">
-      <Greet name={'Pramuditha'} />
+      <Greet name={'Pramuditha'} messageCount={10}/>
     </div>
   );
 }
--- react-typescript-demo/src/components/Greet.tsx
+++ react-typescript-demo/src/components/Greet.tsx
@@ -2,9 +2,10 @@ import React from 'react'
 
 type GreetProps = {
     name: string;
+    messageCount: number;
 }
 export const Greet = (props: GreetProps) => {
   return (
-    <div>Welcome {props.name}</div>
+    <div>Welcome {props.name} you have {props.messageCount} unread messages</div>
   )
 }
